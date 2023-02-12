a8352b32119547d2069ee85bdde557e70533cea5
Hubert Miś <hubert.mis@gmail.com>
2023-02-12 18:18:39 +0100

    Show warnings if documentation of public items is missing

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 #![cfg_attr(not(any(test, doctest)), no_std)]
+#![warn(missing_docs)]
+#![warn(rustdoc::missing_crate_level_docs)]
+#![warn(rustdoc::missing_doc_code_examples)]
 
 //! Radio protocols usage playground for Nordic nRF SoCs.
 //!
