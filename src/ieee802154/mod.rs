0b25cf48e9e1f899cfaa67469b58d7d102166469
Hubert Miś <hubert.mis@gmail.com>
2023-02-15 19:49:20 +0100

    Implement IEEE 802.15.4 PIB (Protocol Information Base)
    IEEE 802.15.4 PIB stores run-time configuration of an IEEE 802.15.4
    instance. It contains configuration of both PHY and MAC layers.
    
    This initial configuration is capable of storing MAC addressing of
    local node.

--- src/ieee802154/mod.rs
+++ src/ieee802154/mod.rs
@@ -2,3 +2,9 @@
 
 /// Module responsible for parsing and building IEEE 802.15.4 frames
 pub mod frame;
+
+/// Module responsible for storing Protocol Information Base
+///
+/// Protocol Information Base includes run-time configuration of both PHY and MAC layers of
+/// IEEE 802.15.4
+pub mod pib;
